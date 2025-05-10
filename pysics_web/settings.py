6a8fc9287dd31029050ce180104d5d532e4f51f9
Nishant <nishant2024rde@gmail.com>
2025-05-10 07:08:14 -0700

    Initial Commit

--- pysics_web/settings.py
+++ pysics_web/settings.py
@@ -116,7 +116,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# === Added: where collectstatic will gather everything ===
+STATIC_ROOT = BASE_DIR / "staticfiles_build"
+
+# If you have additional static dirs (e.g. for global assets), you can list them:
+# STATICFILES_DIRS = [
+#     BASE_DIR / "mainapp" / "static",
+# ]
+# ========================================================
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
