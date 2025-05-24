bfac1897e42d83c9cd0798b991ac4703152e45e0
Nishant <nishant2024rde@gmail.com>
2025-05-23 23:00:35 -0700

    Initial Commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
+    if request.user.is_authenticated:
+        email = request.user.email
+        log_in = True
+        print(log_in, email)
+    else:
+        email = None
+        log_in = False
+        print(log_in, email)
+    
+    # User = get_user_model()
+    # # filter for users who have an unusable password (set by django-google-sso)
+    # qs = User.objects.filter(password__startswith=UNUSABLE_PASSWORD_PREFIX)
+
+    # # pull out the emails
+    # emails = list(qs.values_list('email', flat=True))
+
+    # # log them server-side
+    # for e in emails:
+    #     logger.info(f"Google SSO user email: {e}")
+
+    # print(emails)
+    # # —– OR, if you prefer plain text lines, comment the JsonResponse above and uncomment below:
+    # # return HttpResponse("\n".join(emails), content_type="text/plain")
+    cred = {"log_in":log_in, "email":email}
+    return render(request, "index.html", cred)
 
 def blog(request):
     return render(request, "blog.html")
